00b796768de76448d3f84f218e6297d9eed248fb
wwwyyykkk1 <164482272+wwwyyykkk1@users.noreply.github.com>
2024-04-07 19:24:19 +1000

    Add files via upload

---
+++ toolkit_config.py
@@ -0,0 +1,9 @@
+"""toolkit_config.py
+Project configuration file
+**Do not submit**
+
+"""
+import os
+
+PRJDIR = r"/Users/wyk/Desktop/UNSW/2024 T1/Fins5546/Toolkit/Toolkit/Project1 Total"
+DATADIR = os.path.join(PRJDIR, 'data')
